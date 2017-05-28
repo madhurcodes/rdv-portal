6b1e402cedc9ade3ffea9d45ef99ae8dda76a4b1
Nikhil Chatuvedi <709nikhil@gmail.com>
2017-05-29 00:40:11 +0530

    Added keys for AWS #2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const venueController = require('./controllers/venueController');
 app.set('port', (process.env.PORT || 3001));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/*' ,express.static('client/build'));
+    app.use(express.static('client/build'));
 }
 
 app.use(bodyParser.json());
