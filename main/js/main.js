fb85ff2f38f7d7d478490b49c4a7cb2e5622df6f
Prateek Garg <prateek@alabotech.com>
2017-06-04 19:25:06 +0530

    Final coming soon page

---
+++ main/js/main.js
@@ -0,0 +1,20 @@
+// created by udit01
+
+
+// video = document.getElementById("bg_video")
+//
+// function detectmob() {
+//    console.log(window.innerWidth);
+//      console.log(window.innerHeight);
+//    if($(window).width() <= 1200 || $(window).height() <= 1200) {
+//      return true;
+//    } else {
+//      return false;
+//    }
+// }
+//
+// if (detectmob())
+// {
+//   console.log("video removed");
+//   video.remove();
+// }
--- main/readme.md
+++
@@ -1,3 +0,0 @@
-This is the website which will remain at the domain name until the fully functional website is made.
-
-To replace the current background image with the basetemplate
